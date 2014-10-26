0f931da1a0baf136c293d8982404e465ad1e6f69
DMarby <david@dmarby.se>
2014-10-26 02:36:15 +0100

    Change pagination for digitalocean, add pagination for amazon

--- plugins/amazon.js
+++ plugins/amazon.js
@@ -4,6 +4,7 @@ var colors = require('colors')
 var amazon = module.exports = {
 
   search: function (account, callback) {
+    var result = []
     var params = {
       Filters: [
         {
@@ -15,38 +16,41 @@ var amazon = module.exports = {
       ]
     }
 
-    var result = []
     aws.config.update({ accessKeyId: account.public_token, secretAccessKey: account.token })
     var todo = account.regions.length;
     account.regions.forEach(function (region) {
-      search_region(region, params, function (servers) {
-        servers.forEach(function (server) {
-          server.Instances.forEach(function (instance) {
-            var current_instance = {
-              'id': instance.InstanceId,
-              'name': find_name(instance.Tags),
-              'region': region,
-              'hostname': instance.PublicDnsName ? instance.PublicDnsName : instance.PublicIpAddress,
-              'account': account,
-              'image': instance.ImageId,
-              'ip': instance.PublicIpAddress,
-              'keypair': instance.KeyName,
-              'private-hostname': instance.PrivateDnsName,
-              'private-ip': instance.PrivateIpAddress,
-              'type': instance.InstanceType,
-              'availability-zone': instance.Placement.AvailabilityZone
-            }
-            instance.Tags.forEach(function (tag) {
-              current_instance['tag.' + tag.Key.toLowerCase()] = tag.Value
+      search_region(region, params, function (err, servers) {
+        if (err) {
+         console.log('Something went wrong while searching Amazon %s'.red, err)
+        } else {
+          servers.forEach(function (server) {
+            server.Instances.forEach(function (instance) {
+              var current_instance = {
+                'id': instance.InstanceId,
+                'name': find_name(instance.Tags),
+                'region': region,
+                'hostname': instance.PublicDnsName ? instance.PublicDnsName : instance.PublicIpAddress,
+                'account': account,
+                'image': instance.ImageId,
+                'ip': instance.PublicIpAddress,
+                'keypair': instance.KeyName,
+                'private-hostname': instance.PrivateDnsName,
+                'private-ip': instance.PrivateIpAddress,
+                'type': instance.InstanceType,
+                'availability-zone': instance.Placement.AvailabilityZone
+              }
+              instance.Tags.forEach(function (tag) {
+                current_instance['tag.' + tag.Key.toLowerCase()] = tag.Value
+              })
+              current_instance['security-group'] = []
+              instance.SecurityGroups.forEach(function (sec) {
+                current_instance['security-group'].push(sec.GroupId)
+                current_instance['security-group'].push(sec.GroupName)
+              })
+              result.push(current_instance)
             })
-            current_instance['security-group'] = []
-            instance.SecurityGroups.forEach(function (sec) {
-              current_instance['security-group'].push(sec.GroupId)
-              current_instance['security-group'].push(sec.GroupName)
-            })
-            result.push(current_instance)
           })
-        })
+        }
         todo--;
         if (todo == 0) {
           callback(result);
@@ -68,22 +72,36 @@ var amazon = module.exports = {
   keys: ['id', 'name', 'region', 'hostname', 'account', 'image', 'ip', 'private-ip', 'private-hostname', 'keypair', 'type', 'security-group', 'availability-zone']
 }
 
-var search_region = function (region, params, callback) {
+var search_region = function (region, params, next) {
   var ec2 = new aws.EC2({ region: region })
-  // Todo, check for NextToken and use for pagination, in case of more than 1k servers
-  // this.hasNextPage(), this.nextPage(callback)
-  // Todo, error handling
-  ec2.describeInstances(params, function(err, data) {
-    if (err) {
-      console.log('Something went wrong when searching Amazon: %s'.red, err)
-      return callback([])
-    }
-    if (data.NextToken) {
-      console.log('NextToken found, more servers available')
-    }
-    callback(data.Reservations)
+  var results = []
+  
+  var describeInstances = function (params, done) {
+    ec2.describeInstances(params, function (err, data) {
+      if (err) {
+        return done(error)
+      }
+      
+      results.push(data.Reservations)
+      
+      if (data.NextToken) {
+        params.NextToken = data.NextToken
+        return describeInstances(params, done)
+      }
+      
+      return done()
+    })
+  }
+  
+  describeInstances(params, function (error) {
+    var result = []
+    results.forEach(function (servers) {
+      result = result.concat(servers)
+    })
+    return next(error, result)
   })
 }
+
 
 var find_name = function (tags) {
   var result = tags.filter(function (element) {
--- plugins/digitalocean.js
+++ plugins/digitalocean.js
@@ -1,36 +1,41 @@
 var nautical = require('nautical')
 var util = require('../util')
-var per_page = 25
 var digitalocean = module.exports = {
 
   search: function (account, callback) {
-    var result = []
+    var results = []
     var client = nautical.getClient({ token: account.token })
-    client.droplets.list({ per_page: per_page }, function (err, reply) {
-      if (err) {
-        console.log('Something went wrong when searching DigitalOcean: %s'.red, err)
-        return callback(result)
-      }
-      result = add_to_results(account, result, reply.body.droplets)
-      if (reply.body.meta && reply.body.meta.total && reply.body.meta.total > per_page) {
-        var pages = Math.floor(reply.body.meta.total / per_page) - 1
-        var todo = pages
-        for (var i = 0; i < pages; i++) {
-          client.droplets.list({ page: (i+2), per_page: per_page }, function (err, reply) {
-            if (err) {
-              console.log('Something went wrong when searching DigitalOcean: %s'.red, err)
-            } else {
-              result = add_to_results(account, result, reply.body.droplets) 
-            }
-            todo--
-            if (todo == 0) {
-              callback(result)
-            }
-          })
+    var params = {}
+    var get_servers = function (page, done) {
+      client.droplets.list({ page: page }, function (err, reply) {
+        if (err) {
+          console.log('Something went wrong when searching DigitalOcean: %s'.red, err)
+          return done(err)
         }
-      } else {
-        callback(result)
-      }
+
+        reply.body.droplets.forEach(function (server) {
+          results.push({
+            'id': server.id,
+            'name': server.name,
+            'region': server.region.slug,
+            'hostname': server.networks.v4[0].ip_address,
+            'account': account,
+            'image': server.image.id,
+            'ip': server.networks.v4[0].ip_address,
+            'type': server.size_slug
+          })
+        })
+
+        if ('function' === typeof reply.next) {
+          return get_servers(page+=1, done)
+        }
+
+        return done()
+      })
+    }
+
+    get_servers(1, function (error) {
+      return callback(results)
     })
   },
 
@@ -45,20 +50,4 @@ var digitalocean = module.exports = {
   regions: ['nyc1', 'ams1', 'sfo1', 'nyc2', 'ams2', 'sgp1', 'lon1', 'nyc3', 'ams3'],
 
   keys: util.keys
-}
-
-var add_to_results = function (account, result, results_to_add) {
-  results_to_add.forEach(function (server) {
-    result.push({
-      'id': server.id,
-      'name': server.name,
-      'region': server.region.slug,
-      'hostname': server.networks.v4[0].ip_address,
-      'account': account,
-      'image': server.image.id,
-      'ip': server.networks.v4[0].ip_address,
-      'type': server.size_slug
-    })
-  })
-  return result
 }
