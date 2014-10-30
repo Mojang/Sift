63d02d86108f17432e517276bf0518ae91774a13
Amir Moulavi <amir.moulavi@gmail.com>
2014-10-30 14:31:44 +0100

    Added Frankfurt region for Amazon plugin

--- plugins/amazon.js
+++ plugins/amazon.js
@@ -78,7 +78,7 @@ var amazon = module.exports = {
     util.display(server, index)
   },
 
-  regions: ['ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-west-1', 'sa-east-1', 'us-east-1', 'us-west-1', 'us-west-2'],
+  regions: ['ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'eu-west-1', 'sa-east-1', 'us-east-1', 'us-west-1', 'us-west-2'],
 
   keys: ['id', 'name', 'region', 'hostname', 'account', 'image', 'ip', 'private-ip', 'private-hostname', 'keypair', 'type', 'security-group', 'availability-zone']
 }
